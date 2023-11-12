864c341e361b0839df0a6694a3bb508ce02d41ec
halimahexe <127022084+halimahexe@users.noreply.github.com>
2023-11-12 23:07:19 +0000

    correcting ID to Id

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-let minBtn = document.getElementByID('min-1');
+let minBtn = document.getElementById('#min-1');
 
-let content = document.getElementByClassName('non-menu-1');
+let content = document.getElementsByClassName('.non-menu-1');
 
 function minimise() {
+    console.log('minimise');
     content.classList.add('hidden');
 }
 
 minBtn.addEventListener('click', minimise);
 
-let maxBtn = document.getElementByID('max-1');
+let maxBtn = document.getElementById('#max-1');
 
 function maximise() {
     if (content.classList.contains('hidden')) {

c917cf42dd7107340163a22a9e5966d48dbf1957
halimahexe <127022084+halimahexe@users.noreply.github.com>
2023-11-13 21:56:13 +0000

    attempting universal functions

--- script.js
+++ script.js
@@ -1,35 +1,64 @@
+const minBtns = document.querySelectorAll('.minimise');
+
+minBtns.forEach(minimise(minBtn)) {
+    minBtn.addEventListener('click', minimise() {
+        minBtn.parentElement.parentElement.nextElementSibling.nextElementSibling.classList.add('hidden');
+    })
+}
+
+const maxBtns = document.querySelectorAll('.maximise');
+
+maxBtns.forEach(maximise(maxBtn)) {
+    maxBtn.addEventListener('click', maximise() {
+        let nonMenu = maxBtn.parentElement.parentElement.nextElementSibling.nextElementSibling;
+        if (nonMenu.classList.contains('hidden')) {
+            nonMenu.classList.remove('hidden');
+        }
+    })
+}
+
+const closeBtns = document.querySelectorAll('.close');
+
+closeBtns.forEach(close(closeBtn)) {
+    closeBtn.addEventListener('click',close() {
+        let box = closeBtn.parentElement.parentElement.parentElement;
+        box.remove();
+    })
+}
+
 // minimise function
 
-let minBtn = document.getElementById('min-1');
+// let minBtn = document.getElementById('min-1');
 
-let content = document.querySelector('.non-menu-1');
+// let content = document.querySelector('.non-menu-1');
 
-function minimise() {
-    content.classList.add('hidden');
-}
+// function minimise() {
+//     content.classList.add('hidden');
 
-minBtn.addEventListener('click', minimise);
+// }
 
-// maximise function
+// minBtn.addEventListener('click', minimise);
 
-let maxBtn = document.getElementById('max-1');
+// // maximise function
 
-function maximise() {
-    if (content.classList.contains('hidden')) {
-        content.classList.remove('hidden')
-    }
-}
+// let maxBtn = document.getElementById('max-1');
 
-maxBtn.addEventListener('click', maximise);
+// function maximise() {
+//     if (content.classList.contains('hidden')) {
+//         content.classList.remove('hidden')
+//     }
+// }
 
-// close function
+// maxBtn.addEventListener('click', maximise);
 
-let closeBtn = document.getElementById('close-1');
+// // close function
 
-let box = document.querySelector('.window');
+// let closeBtn = document.getElementById('close-1');
 
-function close() {
-    box.remove();
-}
+// let box = document.querySelector('.window');
 
-closeBtn.addEventListener('click', close);
+// function close() {
+//     box.remove();
+// }
+
+// closeBtn.addEventListener('click', close);

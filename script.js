495f8f51e80ab7e5bd2977ef3e650564651ef1f3
halimahexe <127022084+halimahexe@users.noreply.github.com>
2023-11-14 22:31:19 +0000

    attempting a send button function

--- script.js
+++ script.js
@@ -9,11 +9,26 @@
 //     })
 // })
 
+// Send Function
+
+// let sendBtns = document.querySelectorAll('button[type="submit"]')
+// console.log(sendBtns);
+
+// let newTexts = document.querySelectorAll('.box');
+
+// sendBtns.forEach(function(sendBtn) {
+
+// })
+
+// I want to take text entered in the textarea, press send and append that text to corresponding .conv.
+// It requires a function that 
+
+// Menu Bar Functions
 
 let minBtns = document.querySelectorAll('.minimise');
 
-minBtns.forEach(function(minBtn) {
-    minBtn.addEventListener('click', function(){
+minBtns.forEach(function (minBtn) {
+    minBtn.addEventListener('click', function () {
         minBtn.parentElement.parentElement.nextElementSibling.nextElementSibling.classList.add('hidden');
         minBtn.parentElement.parentElement.parentElement.classList.add('only-mb');
     })
@@ -21,8 +36,8 @@ minBtns.forEach(function(minBtn) {
 
 let maxBtns = document.querySelectorAll('.maximise');
 
-maxBtns.forEach(function(maxBtn) {
-    maxBtn.addEventListener('click', function(){
+maxBtns.forEach(function (maxBtn) {
+    maxBtn.addEventListener('click', function () {
         maxBtn.parentElement.parentElement.nextElementSibling.nextElementSibling.classList.remove('hidden');
         maxBtn.parentElement.parentElement.parentElement.classList.remove('only-mb');
     })
@@ -30,48 +45,48 @@ maxBtns.forEach(function(maxBtn) {
 
 let closeBtns = document.querySelectorAll('.close');
 
-closeBtns.forEach(function(closeBtn) {
-    closeBtn.addEventListener('click', function() {
+closeBtns.forEach(function (closeBtn) {
+    closeBtn.addEventListener('click', function () {
         closeBtn.parentElement.parentElement.parentElement.remove();
     })
 })
 
-// minimise function
+// // minimise function
 
-let minBtn = document.getElementById('min-1');
+// let minBtn = document.getElementById('min-1');
 
-let content = document.querySelector('.non-menu');
+// let content = document.querySelector('.non-menu');
 
-let divWindow = document.querySelector('.window');
+// let divWindow = document.querySelector('.window');
 
-function minimise() {
-    content.classList.add('hidden');
-    divWindow.classList.add('only-mb');
-}
+// function minimise() {
+//     content.classList.add('hidden');
+//     divWindow.classList.add('only-mb');
+// }
 
-minBtn.addEventListener('click', minimise);
+// minBtn.addEventListener('click', minimise);
 
-// maximise function
+// // maximise function
 
-let maxBtn = document.getElementById('max-1');
+// let maxBtn = document.getElementById('max-1');
 
-function maximise() {
-    if (content.classList.contains('hidden')) {
-        content.classList.remove('hidden');
-        divWindow.classList.remove('only-mb');
-    }
-}
+// function maximise() {
+//     if (content.classList.contains('hidden')) {
+//         content.classList.remove('hidden');
+//         divWindow.classList.remove('only-mb');
+//     }
+// }
 
-maxBtn.addEventListener('click', maximise);
+// maxBtn.addEventListener('click', maximise);
 
-// close function
+// // close function
 
-let closeBtn = document.getElementById('close-1');
+// let closeBtn = document.getElementById('close-1');
 
-let box = document.querySelector('.window');
+// let box = document.querySelector('.window');
 
-function close() {
-    box.remove();
-}
+// function close() {
+//     box.remove();
+// }
 
-closeBtn.addEventListener('click', close);
+// closeBtn.addEventListener('click', close);
